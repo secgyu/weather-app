5bb60f3c9df87be480d3b05686a4c2492236bd57
secgyu <abc2232002@gmail.com>
2025-02-08 18:06:41 +0900

    날씨 앱 제작

---
+++ src/app/components/RevalidateButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+type Props = {
+  tag: string;
+};
+
+export default function ReavalidateButton({ tag }: Props) {
+  const handleClick = async () => {
+    const res = await fetch("/api/revalidate?tag=" + tag, { method: "POST" });
+    const result = await res.json();
+    console.log(result);
+  };
+  return <button onClick={handleClick}>캐시 비우기</button>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,30 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import style from "./style.module.css";
+import { getCurrentWeather } from "./utils/getCurrentWeather";
+import { getTime } from "./utils/getTime";
+import ReavalidateButton from "./components/RevalidateButton";
 
-export default function Home() {
+export default async function Home() {
+  const res = await getCurrentWeather("seoul");
+  const time = await getTime(res.location.tz_id);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <h1>날씨 앱</h1>
+      <h3>{time.dateTime}</h3>
+      <ul className={style.list}>
+        <li>
+          <Link href="/seoul?name=서울">서울</Link>
+          <span>{res.current.condition.text}</span>
+        </li>
+        <li>
+          <Link href="/NYC?name=뉴욕">뉴욕</Link>
+        </li>
+        <li>
+          <Link href="/london?name=런던">런던</Link>
+        </li>
+      </ul>
+      <ReavalidateButton tag={"time"} />
+    </>
   );
 }
